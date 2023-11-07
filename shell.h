c997d5905ab8aaa72ef957d4805738bb5dfc9852
George Michael Pussl <george.pussl@gmail.com>
2023-11-06 18:38:39 -0800

    shell.h updates

--- shell.h
+++ shell.h
@@ -12,5 +12,18 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* Function Prototypes */
+void shell_loop(void);
+char *read_line(void);
+char **split_line(char *line);
+int execute(char **args);
+char *_getenv(const char *name);
+char *find_path(char *command, char **env);
+int launch(char **args, char **env);
+void exit_shell(char **args);
+void print_env(char **env);
+void env_shell(char **args, char **env);
+char *own_getline(void);
+void free_tokens(char **tokens);
 
-#endif
+#endif /* SHELL_H */
