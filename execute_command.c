ada6e3ee2088e5274bcee0695b2aaf378a612db3
CharityZawadi <zawadicharity8@gmail.com>
2023-11-14 03:48:17 -0800

    updated

---
+++ execute_command.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+/**
+ * execute_command - Execute the entered command
+ * @command: The command to execute
+ */
+void execute_command(char *command)
+{
+char *path = search_path(args[0]);
+pid_t child_pid;
+int status;
+
+if (path == NULL)
+{
+fprintf(stderr, ":) %s: command not found\n", args[0]);
+return;
+}
+
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+
+if (child_pid == 0)
+{
+if (execve(path, args, NULL) == -1)
+{
+perror("execve");
+exit(EXIT_FAILURE);
+}
+}
+else
+{
+waitpid(child_pid, &status, 0);
+}
+
+free(path);
+}
+
