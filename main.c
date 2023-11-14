253d135553780d5c620e709f14dfe63252af312e
George Michael Pussl <george.pussl@gmail.com>
2023-11-14 05:12:32 -0800

    Simple_Shell

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "shell.h"
 
+
+/**
+ * @brief Main function for the shell.
+ *
+ * This function runs an interactive shell that continuously prompts the user for input,
+ * reads the input, tokenizes it, and executes the corresponding command.
+ *
+ * @return 0 on successful execution.
+ */
+
 int main(void)
 {
 char *input;
