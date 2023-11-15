5b0adb0991ce5eede6054e8fcc995535663e57d4
George Michael Pussl <george.pussl@gmail.com>
2023-11-15 05:15:42 -0800

    Simple_Shell

--- simple_shell.c
+++ simple_shell.c
@@ -2,40 +2,41 @@
 
 int main(void)
 {
-    char *line;
-    size_t len = 0;
-    ssize_t read;
+char *line;
+size_t len = 0;
+ssize_t read;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        read = getline(&line, &len, stdin);
+while (1)
+{
+printf("#cisfun$ ");
+read = getline(&line, &len, stdin);
 
-        if (read == -1)
-        {
-            if (feof(stdin))
-            {
-                printf("\n");
-                free(line);
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                perror("getline");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        line[read - 1] = '\0'; /* Remove the newline character*/
-
-        if (execute_command(line) == -1)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-        }
-
-        free(line);
-        line = NULL;
-    }
-
-    return 0;
+if (read == -1)
+{
+if (feof(stdin))
+{
+printf("\n");
+free(line);
+exit(EXIT_SUCCESS);
 }
+else
+{
+perror("getline");
+exit(EXIT_FAILURE);
+}
+}
+/* Remove the newline character*/
+line[read - 1] = '\0';
+
+if (execute_command(line) == -1)
+{
+fprintf(stderr, "./shell: No such file or directory\n");
+}
+
+free(line);
+line = NULL;
+}
+
+return (0);
+}
+
