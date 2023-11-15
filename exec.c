40fcb6ac10d99ca5f8e71c0acdc3efd0b4d1c2d4
George Michael Pussl <george.pussl@gmail.com>
2023-11-15 06:05:57 -0800

    Simple_Shell

--- exec.c
+++ exec.c
@@ -1,71 +1,19 @@
-##include "simple_shell.h"
-
-void sigint_handler(int signo)
-{
-    (void)signo; // Suppress unused variable warning
-    write(STDERR_FILENO, "\n#cisfun$ ", 10);
-}
-
-int execute_command(char *command)
-{
-    pid_t pid, wpid;
-    int status;
-
-    // Set up signal handler for Ctrl+C
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-    {
-        perror("signal");
-        exit(EXIT_FAILURE);
-    }
-
-    pid = fork();
-
-    if (pid == 0) // Child process
-    {
-        // Check if the command contains a '/'
-        if (strchr(command, '/') == NULL)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-            _exit(EXIT_FAILURE);
-        }
-
-        if (execlp(command, command, (char *)NULL) == -1)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-            _exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0) // Forking error
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else // Parent process
-    {
-        do
-        {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-
-    (void)wpid; // Suppress unused variable warning
-
-    return 0;
-}include "simple_shell.h"
+#include "simple_shell.h"
 
 /**
- * sigint_handler - Handles the SIGINT signal.
- * @signo: The signal number.
- *
- * Description:
- * This function handles the SIGINT signal, providing a simple action
- * (currently doing nothing) when the signal is received.
- */
+* sigint_handler - Handles the SIGINT signal.
+* @signo: The signal number.
+*
+* Description:
+* This function handles the SIGINT signal, providing a simple action
+* (currently doing nothing) when the signal is received.
+*/
+
 
 void sigint_handler(int signo)
 {
 	(void)signo;
-
+	write(STDERR_FILENO, "\n#cisfun$ ", 10);
 }
 
 /**
@@ -78,41 +26,40 @@ void sigint_handler(int signo)
 
 int execute_command(char *command)
 {
-	pid_t pid, wpid;
-	int status;
+pid_t pid, wpid;
+int status;
+if (signal(SIGINT, sigint_handler) == SIG_ERR)
+{
+perror("signal");
+exit(EXIT_FAILURE);
+}
+pid = fork();
+if (pid == 0)
+{
+if (strchr(command, '/') == NULL)
+{
+fprintf(stderr, "./shell: No such file or directory\n");
+_exit(EXIT_FAILURE);
+}
+if (execlp(command, command, (char *)NULL) == -1)
+{
+fprintf(stderr, "./shell: No such file or directory\n");
+_exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else
+{
+do {
+wpid = waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
 
-	if (signal(SIGINT, sigint_handler) == SIG_ERR)
-	{
-		perror("signal");
-		exit(EXIT_FAILURE);
-	}
-	pid = fork();
-	if (pid == 0)
-	{
-		if (strchr(command, '/') == NULL)
-		{
-			fprintf(stderr, "./shell: No such file or directory\n");
-			_exit(EXIT_FAILURE);
-		}
-		if (execlp(command, command, (char *)NULL) == -1)
-		{
-			fprintf(stderr, "./shell: No such file or directory\n");
-			_exit(EXIT_FAILURE);
-		}
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		do {
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
+(void)wpid;
 
-	(void)wpid;
-
-	return (0);
+return (0);
 }
