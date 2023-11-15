41bdd734675171696b6f8e01b2b9e6a33dcc0e96
CharityZawadi <zawadicharity8@gmail.com>
2023-11-15 05:27:11 -0800

    work

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,16 @@
 #include "simple_shell.h"
 
+/**
+ * main - Entry point for the simple shell program.
+ *
+ * Description:
+ * The main function sets up a shell interface, reads and processes user input
+ * in a loop, and executes commands until the user decides to exit.
+ *
+ * Return:
+ * Returns 0 on successful execution.
+ */
+
 int main(void)
 {
 char *line;
