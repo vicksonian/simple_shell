da413841c4bdbbe6fd79078680d5adadc63002fa
George Michael Pussl <george.pussl@gmail.com>
2023-11-15 05:53:20 -0800

    updates

--- execute_command.c
+++ execute_command.c
@@ -1,4 +1,57 @@
-#include "simple_shell.h"
+##include "simple_shell.h"
+
+void sigint_handler(int signo)
+{
+    (void)signo; // Suppress unused variable warning
+    write(STDERR_FILENO, "\n#cisfun$ ", 10);
+}
+
+int execute_command(char *command)
+{
+    pid_t pid, wpid;
+    int status;
+
+    // Set up signal handler for Ctrl+C
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    {
+        perror("signal");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+
+    if (pid == 0) // Child process
+    {
+        // Check if the command contains a '/'
+        if (strchr(command, '/') == NULL)
+        {
+            fprintf(stderr, "./shell: No such file or directory\n");
+            _exit(EXIT_FAILURE);
+        }
+
+        if (execlp(command, command, (char *)NULL) == -1)
+        {
+            fprintf(stderr, "./shell: No such file or directory\n");
+            _exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0) // Forking error
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    else // Parent process
+    {
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    (void)wpid; // Suppress unused variable warning
+
+    return 0;
+}include "simple_shell.h"
 
 /**
 * execute_command - Executes a command with the given arguments.
