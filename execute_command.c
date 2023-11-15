a1cdee84e360fa4631be3c10f59749cb04834358
CharityZawadi <zawadicharity8@gmail.com>
2023-11-15 05:16:24 -0800

    updat

--- execute_command.c
+++ execute_command.c
@@ -1,54 +1,61 @@
 #include "simple_shell.h"
 
+/**
+ * execute_command - Executes a command with the given arguments.
+ * @args: An array of strings representing the command and its arguments.
+ *
+ * Return:
+ * Returns 1 if the command is executed successfully.
+ */
 void sigint_handler(int signo)
 {
-    (void)signo;
+(void)signo;
     
 }
 
 int execute_command(char *command)
 {
-    pid_t pid, wpid;
-    int status;
+pid_t pid, wpid;
+int status;
 
     /* Set up signal handler for Ctrl+C*/
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-    {
-        perror("signal");
-        exit(EXIT_FAILURE);
-    }
+if (signal(SIGINT, sigint_handler) == SIG_ERR)
+{
+perror("signal");
+exit(EXIT_FAILURE);
+}
 
-    pid = fork();
+pid = fork();
 
-    if (pid == 0)
-    {
+if (pid == 0)
+{
         
-        if (strchr(command, '/') == NULL)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-            _exit(EXIT_FAILURE);
-        }
+if (strchr(command, '/') == NULL)
+{
+fprintf(stderr, "./shell: No such file or directory\n");
+_exit(EXIT_FAILURE);
+}
 
-        if (execlp(command, command, (char *)NULL) == -1)
-        {
-            fprintf(stderr, "./shell: No such file or directory\n");
-            _exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        do
-        {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+if (execlp(command, command, (char *)NULL) == -1)
+{
+fprintf(stderr, "./shell: No such file or directory\n");
+_exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else
+{
+do
+{
+wpid = waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
 
-    (void)wpid;
+(void)wpid;
 
-    return 0;
+return 0;
 }
